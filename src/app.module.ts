2a0505ebdbc11d2e20a0dfc3855a604eeed413e1
= <=>
2022-05-23 20:37:48 +0200

    Add a new user controller for signing up.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CoffeesModule } from 'coffee/coffees.module';
 import { CommentsModule } from 'comment/comments.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost'),
     CoffeesModule,
     CommentsModule,
+    UsersModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
